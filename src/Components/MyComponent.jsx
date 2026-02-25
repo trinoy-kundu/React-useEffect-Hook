2c391c22c5b429d258a3d3a5a9a56fdb0e699d52
Trinoy Kundu <trinoykundu@gmail.com>
2026-02-25 14:14:53 +0600

    useEffect run only once

--- src/Components/MyComponent.jsx
+++ src/Components/MyComponent.jsx
@@ -2,28 +2,28 @@ import { useState, useEffect } from "react";
 
 function MyComponent() {
     const [count, setCount] = useState(0);
-    const [text, setText] = useState('');
-    // const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(new Date());
+
+    const tick = () => {
+        setDate(new Date());
+    }
 
     useEffect(() => {
         console.log('updating document title')
         document.title = `Clicked ${count} times`;
-    }, [count])
+    }, [count]);
+
+    useEffect(() => {
+        setInterval(tick, 1000);
+    }, []);
 
     const addCount = () => {
         setCount((prevCount) => prevCount + 1);
     }
 
-    // const tick = () => {
-    //     setDate(new Date());
-    // }
-
     return (
         <div>
-            <p>
-                <input type="text" value={text} onChange={(e) => {setText(e.target.value)}}/>
-            </p>
-            {/* <p>{date.toLocaleTimeString()}</p>   */}
+            <p>{date.toLocaleTimeString()}</p>  
             <button type="button" onClick={addCount}>Click Me</button>
         </div>
     )
